a02e37b64784e5b27ff0bef91c7b8bf45c3791b4
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-04-12 11:11:13 +0200

    Add validaciones en el servidor

--- www/scripts/add_comment.php
+++ www/scripts/add_comment.php
@@ -2,6 +2,18 @@
     include("bd.php");
     $uri = $_SERVER['REQUEST_URI'];
 
-    addComment($_GET['ev'], $_POST['comment'], $_POST['name']);
+    $name = $_POST['name'];
+    $comment = $_POST['comment'];
+    $email = $_POST['email'];
+
+    if(!isset($_GET['ev']) 
+        || !filter_var($email, FILTER_VALIDATE_EMAIL) 
+        || empty($comment)
+        || empty($name)
+    ){
+        return;
+    }
+
+    addComment($_GET['ev'], $comment, $name);
     header('Location:/evento/' . $_GET['ev']);
 ?>
