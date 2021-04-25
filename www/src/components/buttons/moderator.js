2901e9006a2a562b58eba9b13c122b5e5d3f5c37
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-04-25 16:21:21 +0200

    Add: change buttons when I press edit and set the focus

--- www/src/components/buttons/moderator.js
+++ www/src/components/buttons/moderator.js
@@ -1,12 +1,24 @@
 
 //Get the button
+const commentsArr    = Array.from(document.getElementsByClassName("comment-content"))
 const editButtons = Array.from(document.getElementsByClassName("edit-button"))
+const saveButtons = Array.from(document.getElementsByClassName("save-button"))
 
-const sayHi = (a,e) => {
-    console.log(a,e);
-    
-}
-const editComment = (e,i) => {
-    console.log(e,i);
-    
+
+const editComment = ( index ) => {
+    const content = commentsArr[index-1]
+    content.contentEditable = true
+    content.focus()
+
+    // Change button
+    const editButton = editButtons[index-1]
+    const saveButton = saveButtons[index-1]
+    editButton.classList.add('display-none')
+    saveButton.classList.remove('display-none')
+
+    // Edit note
+    const node = document.createElement("p")
+    node.classList.add("comment-edited")
+    node.innerHTML = "Editado por el moderador"
+    content.insertAdjacentElement("afterend",node)
 }
